c6efe13e1d7027aa33d03b50df3086ffd77ced60
Uros98 <uros.milovanovic98@gmail.com>
2021-11-30 22:59:15 +0100

    Dodata isum strana

--- src/pages/isum-page/isum-page.component.jsx
+++ src/pages/isum-page/isum-page.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./isum-page.styles.scss";
 import Header from "../../component/header/header.component";
-import IsumMockup from "../../assets/mockup/isum-mockup-page-v2.png";
+// import IsumMockup from "../../assets/mockup/isum-mockup-page-v2.png";
 
 
 const IsumPage = () => {
@@ -21,17 +21,39 @@ const IsumPage = () => {
                 <div className="isum-container">
                     <div className="isum-info">
                         <div className="info">
-                            <h2>Technology:</h2>
+                            <h2>Technology</h2>
                             <p>Angular, Spring </p>
                         </div>
+                        <div className="border">
+                        </div>
                         <div className="info">
-                            <h2>Tools:</h2>
+                            <h2>Tools</h2>
                             <p>Webstorm, IntelliJ, DataGrip</p>
                         </div>
+                        <div className="border">
+                        </div>
                         <div className="info">
-                            <h2>Timeline:</h2>
+                            <h2>Timeline</h2>
                             <p>Jan 2021 - Jan 2022</p>
                         </div>
+                    </div>
+                    <div className="isum-overview">
+                        <h2>Overview</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque consectetur consequatur
+                            cupiditate dolore dolores enim facere, fugit, hic id illum laudantium nesciunt, numquam
+                            praesentium saepe sequi tempora vel voluptatum!</p>
+                    </div>
+                    <div className="isum-problems">
+                        <h2>Problems</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque consectetur consequatur
+                            cupiditate dolore dolores enim facere, fugit, hic id illum laudantium nesciunt, numquam
+                            praesentium saepe sequi tempora vel voluptatum!</p>
+                    </div>
+                    <div className="isum-solution">
+                        <h2>Solution</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque consectetur consequatur
+                            cupiditate dolore dolores enim facere, fugit, hic id illum laudantium nesciunt, numquam
+                            praesentium saepe sequi tempora vel voluptatum!</p>
                     </div>
                 </div>
 
